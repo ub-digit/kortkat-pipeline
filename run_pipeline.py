13db32bc81a6d1c44efdd57bf4f8070c322f9f2b
Magnus Kull <magnus.kull@ub.gu.se>
2026-03-05 18:26:37 +0100

    json as output format from match

--- run_pipeline.py
+++ run_pipeline.py
@@ -79,7 +79,7 @@ def define_pipeline_steps():
             "key": "match",
             "name": "Match extracted data against dataset",
             "command": (
-                lambda args: ["cargo", "run", "--release", "--", "-c", "match-json-zip", "-s", "libris-v1_7", "-i", str(args["pipeline_directory"]) + "/post-process", "-o", str(args["pipeline_directory"]) + "/match/outputfile.xlsx", "-F", "xlsx", "-C", str(args["pipeline_directory"]) + "/config.json"]
+                lambda args: ["cargo", "run", "--release", "--", "-c", "match-json-zip", "-s", "libris-v1_7", "-i", str(args["pipeline_directory"]) + "/post-process", "-o", str(args["pipeline_directory"]) + "/match/outputfile.json", "-F", "json", "-C", str(args["pipeline_directory"]) + "/config.json"]
             ),
             "working_dir": MATCH_WORKING_DIR
         }
